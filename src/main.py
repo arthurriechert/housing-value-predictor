6998a6c3eb9702581fb186b24cb2591a9eb4c61e
Arthur Riechert <riechertarthur@gmail.com>
2023-06-06 13:55:27 -0400

    Added test run to main

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import model.linear_regression_model as lrm
+
+if __name__ == "__main__":
+    """
+    Running simple test
+    
+    Run:
+        python main.py
+
+    """
+
+    lrm.test_model()
