31f52f8f0f2f2e2bf670cfa4b9d43eca2bdd4c16
Arthur Riechert <riechertarthur@gmail.com>
2023-06-08 19:44:58 -0400

    Replaced manual assignment with to_numpy()

--- src/model/load_data.py
+++ src/model/load_data.py
@@ -31,28 +31,16 @@ def get_training_sets ():
 
     # Set number of training examples, m, and features, n
     m, n = data.shape[0], 8
-
-    # Initialize placeholder array
-    X_train = np.empty((m, n), dtype=np.float64)
     
     # Organize features
-    X_train[:, 0] = data.longitude
-    X_train[:, 1] = data.latitude
-    X_train[:, 2] = X_train[:, 0] * X_train[:, 1]
-    X_train[:, 3] = data.housing_median_age
-    X_train[:, 4] = data.total_rooms
-    X_train[:, 5] = data.median_income
-    X_train[:, 6] = data.total_bedrooms
-    X_train[:, 7] = data.population
-
-    # Print diagnostics
-    print(f"DATA: \n{X_train}")
+    data["longitude x latitude"] = data["longitude"] * data["latitude"]
+    X_train = data[["longitude", "latitude", "longitude x latitude", "housing_median_age", "total_rooms", "median_income", "total_bedrooms", "population"]].to_numpy()
 
     # Save some data for evaluation
     m_limit = 15000
 
     # Output will be house value
-    Y_train = np.array(data.median_house_value, dtype=np.float32)
+    Y_train = data["median_house_value"].to_numpy()
 
     return X_train[m_limit:,:], Y_train[m_limit:]
 
@@ -72,26 +60,16 @@ def get_evaluation_data (m_limit):
     # Set number of training examples, m, and features, n
     m, n = data.shape[0], 8
 
-    # Initialize placeholder
-    X_train = np.empty((m, n), dtype=np.float64)
-    
-    # Organize features
-    X_train[:, 0] = data.longitude
-    X_train[:, 1] = data.latitude
-    X_train[:, 2] = X_train[:, 0] * X_train[:, 1]
-    X_train[:, 3] = data.housing_median_age
-    X_train[:, 4] = data.total_rooms
-    X_train[:, 5] = data.median_income
-    X_train[:, 6] = data.total_bedrooms
-    X_train[:, 7] = data.population
+     # Organize features
+    data["longitude x latitude"] = data["longitude"] * data["latitude"]
+    X_train = data[["longitude", "latitude", "longitude x latitude", "housing_median_age", "total_rooms", "median_income", "total_bedrooms", "population"]].to_numpy()
 
-    # Print diagnostics
+    # Output will be house value
+    Y_train = data["median_house_value"].to_numpy()
+
     print(f"DATA: \n{X_train}")
 
     # Save some data for evaluation
     m_limit = m - m_limit
-
-    # Output will be house value
-    Y_train = np.array(data.median_house_value, dtype=np.float32)
 
     return X_train[:m_limit,:], Y_train[:m_limit]
